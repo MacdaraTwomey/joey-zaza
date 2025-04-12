3d8ea13493844befe18483466d5a0f8d4032eb5d
Macdara Twomey <macdara.twomey@gmail.com>
2025-04-12 14:08:45 +0800

    working non-alpha image splatting

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 
 mkdir -p build
 cd build
-cl.exe -std:c++latest /Zc:preprocessor -Zc:strictStrings- -W3 -wd5105 -wd4201 -wd4996 -wd4505 -INCREMENTAL:NO -FC -EHs- -nologo \
-	-Zi ..\\src\\zaza.cpp /link Ole32.lib Shell32.lib
+cl.exe -std:c++latest /Zc:preprocessor -Zc:strictStrings- -O2 -W3 -wd5105 -wd4201 -wd4996 -wd4505 -INCREMENTAL:NO -FC -EHs- -nologo \
+	-Zi -fsanitize=address  ..\\src\\zaza.cpp /link Ole32.lib Shell32.lib /INFERASANLIBS
--- src/zaza.cpp
+++ src/zaza.cpp
@@ -3,17 +3,22 @@
 #include <stdio.h>
 #include <limits.h>
 #include <stdlib.h>
+#include <time.h>
+#include <assert.h>
 
 #include <Shobjidl.h>
 #include <Shlobj.h>
 #include <winbase.h>
 
+//#define STBI_WINDOWS_UTF8
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+//#define STBIW_WINDOWS_UTF8
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
 #include "logo.h"
+#include "joey-logo.h"
 
 int main(int ArgCount, char **Args) 
 {
@@ -115,52 +120,103 @@ int main(int ArgCount, char **Args)
             int Height = 0;
             int ChannelCount = 0;
             int DesiredComponents = 4;
-            // TODO: The last parameter of this is 'wrong'
             unsigned char *Data = stbi_load(ExistingWallpaperPath8, &Width, &Height, &ChannelCount, DesiredComponents);
             printf("Got image data, x = %d, y = %d, n = %d\n", Width, Height, ChannelCount);
             
             float logo_fraction = (float)LOGO_WIDTH / Width;
             
-            
+            srand((int)time(NULL));
             float t = (float)rand() / RAND_MAX;
-            printf("t %f\n", t);
+            //printf("t %f\n", t);
             
             int MinX = LOGO_WIDTH;
             int MaxX = Width - LOGO_WIDTH;
             
-            int Pos = (int)((1.0f - t) * MinX + (t * MaxX));
+            t = 0.5f;
+            int PosX = (int)((1.0f - t) * MinX + (t * MaxX));
+            int PosY = Height / 2;
             
             int Stride = Width * DesiredComponents;
             
-#if 0
-            unsigned char *Ptr = Data + Pos + (Pos * Stride);
-            unsigned int *LogoPtr = LogoBytes;
-            for (int y = 100; y < 100 + LOGO_HEIGHT; ++y) {
-                for (int x = Pos; x < Pos + LOGO_WIDTH; ++x) {
-                    unsigned int *Ptr
+#if 1
+            unsigned int *LogoPtr = (unsigned int *)JoeyLogo;
+            for (int y = 0; y < JOEY_LOGO_WIDTH; ++y) {
+                unsigned int *Ptr = (unsigned int *)(Data + (PosX * DesiredComponents) + ((PosY + y) * Stride));
+                for (int x = 0; x < JOEY_LOGO_WIDTH; ++x) {
+                    //                         AARRGGBB
+                    // unsigned int Colour = 0xFF00FF00; 
+                    unsigned int Colour = *LogoPtr++;
+                    //unsigned ColourABGR = Colour 
+                    *Ptr++ = Colour;
                 }
             }
 #endif
+            // stb will write expecting RGBA
             
-            stbi_write_png_compression_level = 0;
-            
-            int WriteRet = stbi_write_png(NewWallpaperPath8, Width, Height, ChannelCount, Data, Stride);
-            printf("write ret %d\n", WriteRet);
-            
-            
-            wchar_t NewWallpaperPath16[4096] = {};
-            int CharacterCount = MultiByteToWideChar(CP_UTF8, 0, 
-                                                     (char *)NewWallpaperPath8, (int)NewWallpaperPathLength, 
-                                                     NewWallpaperPath16, (int)sizeof(NewWallpaperPath16));
-            NewWallpaperPath16[CharacterCount] = 0;
-            wprintf(L"NewWallpaperPath16 %ws\n", NewWallpaperPath16);
-            
-            Result = DesktopWallpaperObj->SetWallpaper(NULL, NewWallpaperPath16);
-            if (Result == S_OK) {
-                printf("SetWallpaper success\n");
+            stbi_write_png_compression_level = 8;
+
+            if (0)
+            {
+                int Width = 0;
+                int Height = 0;
+                int ChannelCount = 0;
+                int DesiredComponents = 4;
+                unsigned int *Data = (unsigned int *)stbi_load("c:\\dev\\projects\\joey-zaza\\joey2.png", &Width, &Height, &ChannelCount, DesiredComponents);
+
+                unsigned char *Ptr = (unsigned char *)Data;
+                
+                if (0) {
+                    printf("#define JOEY_LOGO_WIDTH 221\n");
+                    printf("#define JOEY_LOGO_HEIGHT 221\n");
+                    printf("#define JOEY_LOGO_COMPONENTS 4\n");
+                    printf("unsigned char JoeyLogo[] = {\n");
+                    for (int y = 0; y < Height; ++y) {
+                        for (int x = 0; x < Width; ++x) {
+                            printf("0x%02hhx, ", *Ptr++);
+                            printf("0x%02hhx, ", *Ptr++);
+                            printf("0x%02hhx, ", *Ptr++);
+                            printf("0x%02hhx, ", *Ptr++);
+                        }
+                        printf("\n");
+                    }
+                    printf("};\n");
+                }
+                
+                int WriteRet = stbi_write_png("c:\\Users\\mac\\.temp2\\joey-logo.png", JOEY_LOGO_WIDTH, JOEY_LOGO_HEIGHT, 4, JoeyLogo, JOEY_LOGO_WIDTH * 4);
+                //int WriteRet = stbi_write_png("c:\\Users\\mac\\.temp2\\joey-logo.png", Width, Height, 4, Data, Width * 4);
+                printf("write ret %d\n", WriteRet);
             }
-            else {
-                printf("SetWallpaper failed\n");
+
+            int WriteRet = stbi_write_png("c:\\Users\\mac\\.temp2\\joey-logo.png", Width, Height, DesiredComponents, Data, Stride);
+            printf("write ret %d\n", WriteRet);
+
+
+            //assert(sizeof(joey2) == sizeof(LogoBytes));
+            //int different = memcmp(joey2, LogoBytes, sizeof(joey2));
+            //printf("different = %d\n", different);
+            //int WriteRet = stbi_write_png("c:\\Users\\mac\\.temp2\\joey2.png", JOEY2_WIDTH, JOEY2_HEIGHT, 4, joey2, JOEY2_WIDTH * 4);
+            //printf("write ret %d\n", WriteRet);
+            //WriteRet = stbi_write_png("c:\\Users\\mac\\.temp2\\LogoBytes.png", LOGO_WIDTH, LOGO_HEIGHT, LOGO_COMPONENTS, LogoBytes, LOGO_WIDTH * LOGO_COMPONENTS);
+            
+            //int WriteRet = stbi_write_png(NewWallpaperPath8, Width, Height, DesiredComponents, Data, Stride);
+            //printf("write ret %d\n", WriteRet);
+            
+            
+            if (0) {
+                wchar_t NewWallpaperPath16[4096] = {};
+                int CharacterCount = MultiByteToWideChar(CP_UTF8, 0, 
+                                                        (char *)NewWallpaperPath8, (int)NewWallpaperPathLength, 
+                                                        NewWallpaperPath16, (int)sizeof(NewWallpaperPath16));
+                NewWallpaperPath16[CharacterCount] = 0;
+                wprintf(L"NewWallpaperPath16 %ws\n", NewWallpaperPath16);
+                
+                Result = DesktopWallpaperObj->SetWallpaper(NULL, NewWallpaperPath16);
+                if (Result == S_OK) {
+                    printf("SetWallpaper success\n");
+                }
+                else {
+                    printf("SetWallpaper failed\n");
+                }
             }
         }
     }
