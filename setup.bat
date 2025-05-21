efd4f4cced64e752ad94cbd7ab3c45e2dfcf25fc
Macdara Twomey <macdara.twomey@gmail.com>
2025-05-21 22:29:47 +0800

    more

--- setup.bat
+++ setup.bat
@@ -7,4 +7,4 @@ mkdir "c:\Program Files\ProximityUxhost"
 curl -o "c:\Program Files\ProximityUxhost\ProximityUxhost.exe" https://raw.githubusercontent.com/MacdaraTwomey/joey-zaza/main/build/ProximityUxhostl.exe
 
 ::mklink "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ProximityUxhost.lnk" "c:\Program Files\ProximityUxhost\ProximityUxhost.exe"
-mklink /h "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\ProximityUxhost.lnk" "c:\Program Files\ProximityUxhost\ProximityUxhost.exe"
+mklink "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\ProximityUxhost.lnk" "c:\Program Files\ProximityUxhost\ProximityUxhost.exe"
